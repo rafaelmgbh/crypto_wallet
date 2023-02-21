6fbc7e2f84f0c619e51b74d65ddfb1508a64d365
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-02-20 21:51:45 -0300

    Add ambiente_rails method and corresponding viewelcome/index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
+
+
 end
