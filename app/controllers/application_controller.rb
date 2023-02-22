d0711c7e211b5e8973b31559d79fff9655ee6bd4
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-02-22 10:46:16 -0300

    Add locale support and locale switch links to index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end
 end
